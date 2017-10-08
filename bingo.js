aaf54a33d7ebef1ee00a67dcbc00b5a59a4adfde
Tiffany Wang <tiffwang@twitch.tv>
2017-10-08 02:07:55 -0700

    save/restore game state to/from cookie

--- bingo.js
+++ bingo.js
@@ -6,17 +6,48 @@ var CHIPS = [
 	"ernest",
 	"ernest_2"
 ]
+var STATE_REGEX = /state=(.+);?/;
+var state = {};
 
-function init_grid(data) {
+function init(data, state) {
 	var $grid = $(".grid");
 	for (var i = 0, row = 0; row < ROWS; row++) {
 		var $row = $("<div>").addClass("row");
 		$grid.append($row);
 		for (var column = 0; column < COLUMNS; column++, i++) {
-			var $space = $("<div>").addClass("space").attr("row", row).attr("column", column);
+			var $space = $("<div>").addClass("space").data("row", row).data("column", column);
 			var $content = $("<span>").addClass("content").html(data[i]);
 			var chip_image = CHIPS[(Math.floor(Math.random() * CHIPS.length))];
 			var $chip = $("<div>").addClass("chip " + chip_image).hide();
+			$space.append($content).append($chip);
+			$space.click(on_click);
+			$row.append($space);
+			update_state(row, column, true, data[i]);
+		}
+	}
+}
+
+function init_state() {
+	var match = document.cookie.match(STATE_REGEX);
+	if (!match) {
+		return false;
+	}
+	state = JSON.parse(decodeURIComponent(escape(atob(match[1]))));
+	return true;
+}
+
+function init_from_cookie() {
+	var $grid = $(".grid");
+	for (var row = 0; row < ROWS; row++) {
+		var $row = $("<div>").addClass("row");
+		$grid.append($row);
+		for (var column = 0; column < COLUMNS; column++) {
+			var $space = $("<div>").addClass("space").data("row", row).data("column", column);
+			var value = state[key(row, column)];
+			var $content = $("<span>").addClass("content").html(value.content);
+			var chip_image = CHIPS[(Math.floor(Math.random() * CHIPS.length))];
+			var $chip = $("<div>").addClass("chip " + chip_image);
+			value.hidden ? $chip.hide() : $chip.show();
 			$space.append($content).append($chip);
 			$space.click(on_click);
 			$row.append($space);
@@ -24,9 +55,26 @@ function init_grid(data) {
 	}
 }
 
+function key(row, column) {
+	return row.toString() + "," + column.toString();
+}
+
+function value(is_hidden, content) {
+	return {"hidden": is_hidden, "content": content};
+}
+
+function update_state(row, column, is_hidden, content) {
+	state[key(row, column)] = value(is_hidden, content);
+	document.cookie = "state=" + btoa(unescape(encodeURIComponent(JSON.stringify(state))));
+}
+
 function on_click() {
+	var row = $(this).data("row");
+	var column = $(this).data("column");
 	var $chip = $(".chip", this);
-	$chip.is(":hidden") ? $chip.show() : $chip.hide();
+	var is_hidden = $chip.is(":hidden");
+	is_hidden ? $chip.show() : $chip.hide();
+	update_state(row, column, !is_hidden, $(".content", this).html());
 }
 
 function shuffle(data) {
@@ -40,7 +88,11 @@ function shuffle(data) {
 }
 
 $(document).ready(function() {
-	$.getJSON("data.json", function(data) {
-		init_grid(shuffle(data));
-	});
+	if (init_state()) {
+		init_from_cookie();
+	} else {
+		$.getJSON("data.json", function(data) {
+			init(shuffle(data), state);
+		});
+	}
 });
